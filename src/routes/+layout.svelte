8f150302b7d785b94b31aede22f63f1b6250cea5
Fedor <vanteevfedor2008@gmail.com>
2023-05-24 15:36:52 +0800

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,67 +12,44 @@
     <!-- Pages will be injected below -->
     <slot></slot>
   </div>
-  <script>
-   const nav = [
-  {
-    title: 'Обо мне',
-    href: '/about',
-    isShow: true,
-  },
-  {
-    title: 'Блог',
-    href: '/blog',
-    isShow: true,
-  },
-  {
-    title: 'Портфолио',
-    href: '/project',
-    isShow: true,
-  },
-  {
-    title: 'Контакты',
-    href: '/contact',
-    isShow: true,
-  },
-  {
-    title: 'temp',
-    href: '/temp',
-    isShow: false,
-  },
-];
-</script>
-
-  <style>
-    .container {
-      max-width: 1400px;
-      margin: 50px auto;
-    }
-
-  </style>
-  <svelte:head>
-    <title>Имя Фамилия</title>
-    </svelte:head>
-    <div class='container'>
-        <div class='info'>
-            <div class='animate-background'>
-                <div class='container info-container'>
-                    <div class='info-text'>
-                        <h1>
-                            <p class='animate-text-1'>Lorem ipsum dolor sit,</p>
-                            <p class='text-2'>Lorem ipsum dolor sit amet.</p>
-                        </h1>
-                    </div>
-                    <h2 class='subtitle'>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto aut, cupiditate dolores
-                        ducimus expedita labore nemo odio quibusdam quidem sapiente soluta.
-                    </h2>
-                    <div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
-                        <a href='/about'>Click Here</a>
-                        <div class='arrow'>→</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  
+ 
     
-    <div class='background-div'></div>
+    
+    <script>
+      const nav = [
+     {
+       title: 'Обо мне',
+       href: '/about',
+       isShow: true,
+     },
+     {
+       title: 'Блог',
+       href: '/blog',
+       isShow: true,
+     },
+     {
+       title: 'Портфолио',
+       href: '/project',
+       isShow: true,
+     },
+     {
+       title: 'Контакты',
+       href: '/contact',
+       isShow: true,
+     },
+     {
+       title: 'temp',
+       href: '/temp',
+       isShow: false,
+     },
+   ];
+   </script>
+   
+     <style>
+       .container {
+         max-width: 1400px;
+         margin: 50px auto;
+       }
+   
+     </style>
