c6bad5604ef57b05e6473c602f22d4f638c4fa1b
Fedor <vanteevfedor2008@gmail.com>
2023-05-24 16:51:41 +0800

    init

---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,6 @@
+<h1>My Second Blog Post</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque suscipit consectetur quaerat esse maiores. Explicabo error ea aliquid numquam quidem doloribus cumque, assumenda libero similique blanditiis, omnis, aliquam eius veniam!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad enim in doloribus dolorem non facere! Minima asperiores porro dolorem non rem pariatur ducimus eos, optio perferendis deleniti enim inventore reiciendis.</p>
+
+
